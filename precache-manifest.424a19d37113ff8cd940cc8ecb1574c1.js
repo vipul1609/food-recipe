8198ffda66ea79cd600c988a9c86995a819dc87b
vipul1609 <vipulvaibhav1609@gmail.com>
2020-04-22 19:00:31 +0530

    Updates

---
+++ precache-manifest.424a19d37113ff8cd940cc8ecb1574c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad04bd9239c0237ce1c93f07af57b2e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "17e3fbfc1d7dc44822e1",
+    "url": "/static/css/main.e8ac8bf9.chunk.css"
+  },
+  {
+    "revision": "f7d3d25b1b879d3b213f",
+    "url": "/static/js/2.bcbc4d85.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bcbc4d85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17e3fbfc1d7dc44822e1",
+    "url": "/static/js/main.f19a7952.chunk.js"
+  },
+  {
+    "revision": "aa54ad6069f1aa6c99f0",
+    "url": "/static/js/runtime-main.cf482f84.js"
+  }
+]);
