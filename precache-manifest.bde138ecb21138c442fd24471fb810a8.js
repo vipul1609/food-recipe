1f97249c72d3780a8154d400512543500f207341
Deep Diwakar <deepdiwakar9@gmail.com>
2020-04-22 23:27:24 +0530

    Updates

---
+++ precache-manifest.bde138ecb21138c442fd24471fb810a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a277a403ab560a5c7c526a787ee05aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f1d4a23bdc5edf556b0c",
+    "url": "/static/css/main.ee1fa9e9.chunk.css"
+  },
+  {
+    "revision": "f7d3d25b1b879d3b213f",
+    "url": "/static/js/2.bcbc4d85.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bcbc4d85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1d4a23bdc5edf556b0c",
+    "url": "/static/js/main.bf3670a5.chunk.js"
+  },
+  {
+    "revision": "aa54ad6069f1aa6c99f0",
+    "url": "/static/js/runtime-main.cf482f84.js"
+  }
+]);
